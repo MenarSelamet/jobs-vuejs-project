be4b180234a8b2aea7758ba1236e1b564ad95540
Menar Selamet <menarselamet007@gmail.com>
2024-07-15 01:01:11 +0300

    Adding Card wrapper component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import Hero from './components/Hero.vue'
-import HomeCards from './components/HomeCards.vue';
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 </script>
 
 <template>
-  <Navbar/>
-  <Hero title="Become a Vue Developer" subTitle="Find the Job that fits your Needs and Expertise"/>
-  <HomeCards/>
+  <Navbar />
+  <Hero
+    title="Become a Vue Developer"
+    subTitle="Find the Job that fits your Needs and Expertise"
+  />
+  <HomeCards />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
