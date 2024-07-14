9f5610fe13bf917a4b4729dcf02652698bc9ab8c
Menar Selamet <menarselamet007@gmail.com>
2024-07-15 00:47:33 +0300

    Static Home Cards created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
 import Hero from './components/Hero.vue'
+import HomeCards from './components/HomeCards.vue';
 </script>
 
 <template>
   <Navbar/>
   <Hero title="Become a Vue Developer" subTitle="Find the Job that fits your Needs and Expertise"/>
+  <HomeCards/>
 </template>
 
 <style scoped>
