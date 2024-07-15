e6547fd72cbc47425a040061d07bf92818456512
Menar Selamet <menarselamet007@gmail.com>
2024-07-15 19:43:29 +0300

    Fixing grid problem

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -15,12 +15,12 @@ defineProps({
 
 <template>
   <Section class="bg-blue-50 px-4 py-10">
-    <div class="container-xl lg:container mx-auto">
-      <h2 class="text-3xl font-bold text-gray-500 mb-6 text-center">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
 
-      <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
           v-for="job in jobs.slice(0, limit || jobs.length)"
           :key="job.id"
@@ -37,3 +37,4 @@ defineProps({
     </div>
   </Section>
 </template>
+
